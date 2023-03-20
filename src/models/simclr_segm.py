786507b9ddbe9407db26771b732c24e7665e6c7c
Vladyslav Zalevskyi <vzalevskyi24@gmail.com>
2023-03-20 11:14:27 +0100

    Multi task sst dump

--- src/models/simclr_segm.py
+++ src/models/simclr_segm.py
@@ -1,10 +1,11 @@
-import pytorch_lightning as pl
 import torch
+import torchmetrics
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
+import pytorch_lightning as pl
+import torch.nn.functional as F
 from monai.networks.nets.resnet import resnet18
-
+from monai.losses import DiceLoss
 from src.models.unet3d.model_encoders import UNet3D
 
 
@@ -20,6 +21,7 @@ class SimCLR(pl.LightningModule):
                  encoder: UNet3D,
                  optimizer: torch.optim.Optimizer,
                  scheduler: torch.optim.lr_scheduler,
+                 dice_weight: float = 0.5,
                  ):
         """_summary_
 
@@ -29,13 +31,18 @@ class SimCLR(pl.LightningModule):
             lr (float): _description_
             img_dim (int): _description_
             temperature (float): _description_
+            dice_weight (float): Weight of the dice term in the loss function.
+                Between 0 and 1. Default: 0.5.
             weight_decay (float): _description_
             encoder (UNet3D): _description_
             optimizer (torch.optim.Optimizer): _description_
             scheduler (torch.optim.lr_scheduler): _description_
         """
         super().__init__()
-        self.save_hyperparameters(ignore=['net'])
+        self.save_hyperparameters(ignore=['net', 'encoder'])
+
+        self.dice_weight = dice_weight
+        self.learning_rate = lr
 
         assert self.hparams.temperature > 0.0, 'The temperature must be a positive float!'
 
@@ -46,45 +53,52 @@ class SimCLR(pl.LightningModule):
         # calculate u-net embeding dimension after max pooling
         embed_dim = int(last_layer_kernels * ((img_dim/(2)**(number_of_layers - 1))**3)/(2**3))
 
-        self.learning_rate = lr
-        # self.mlp_head = resnet18(n_input_channels=1, spatial_dims=3,  pretrained=False)
-        # pretrain = torch.load('/mrhome/vladyslavz/Downloads/nobackup/MedicalNet_pytorch_files2/pretrain/resnet_18.pth')
-        # pretrain['state_dict'] = {k.replace('module.', ''): v for k, v in pretrain['state_dict'].items()}
-        # self.mlp_head.load_state_dict(pretrain['state_dict'], strict=False)
-        # self.mlp_head.fc = nn.Sequential(nn.Linear(512, 512),
-                                            # nn.ReLU(inplace=False),
-                                            # nn.Linear(512, 256))
+        self.unet = encoder
 
         # The MLP for g(.) consists of Linear->ReLU->Linear
-        self.mlp_head = nn.Sequential(encoder,
-                                      nn.MaxPool3d(kernel_size=2, stride=2),
+        self.mlp_head = nn.Sequential(nn.MaxPool3d(kernel_size=2, stride=2),
                                       nn.Flatten(),
                                       nn.Linear(embed_dim, embed_dim),
                                       nn.ReLU(inplace=False),
-                                      nn.Linear(embed_dim, 256))
+                                      nn.Linear(embed_dim, hidden_dim))
 
-        self.criterion = torch.nn.CrossEntropyLoss()
+        # define the loss functions for segmentation and contrastive learning
+        self.contrastive_loss = torch.nn.CrossEntropyLoss()
+
+        self.dice_loss = DiceLoss(to_onehot_y=True, softmax=True,
+                                  include_background=False)
+
+        self.val_dsc = torchmetrics.Dice(ignore_index=0,
+                                         average='macro',
+                                         num_classes=3,
+                                         multiclass=True)
+        self.train_dsc = torchmetrics.Dice(ignore_index=0,
+                                           average='macro',
+                                           num_classes=3,
+                                           multiclass=True)
 
     def configure_optimizers(self):
-        optimizer = optim.SGD(self.parameters(), lr=0.00001)
+        optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
 
-    def forward(self, x):
-        return self.mlp_head(x)
-
     def info_nce_loss(self, batch, mode='train'):
-        imgs, _ = batch
-        imgs = torch.cat(imgs, dim=0)
+
+        # NEW UN-BATCHING WITH LABELS
+        img1, img2, lab1, lab2 = batch
+        pred_segm, bottle_neck_features = self.unet(torch.concat([img1, img2]))
+        embeddings = self.mlp_head(bottle_neck_features)
+        ########### InfoNCE Loss ###########
+
         # Encode all images
-        features = self.mlp_head(imgs)
-        
-        labels = torch.cat([torch.arange(batch[-1].shape[0], device=features.device) for i in range(2)], dim=0)
+        # features = self.mlp_head(imgs)
+
+        labels = torch.cat([torch.arange(batch[-1].shape[0], device=embeddings.device) for i in range(2)], dim=0)
         labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
 
-        features = F.normalize(features, dim=1)
-        
-        similarity_matrix = torch.matmul(features, features.T)
-        
+        embeddings = F.normalize(embeddings, dim=1)
+
+        similarity_matrix = torch.matmul(embeddings, embeddings.T)
+
         # discard the main diagonal from both: labels and similarities matrix
         mask = torch.eye(labels.shape[0], dtype=torch.bool, device=similarity_matrix.device)
         labels = labels[~mask].view(labels.shape[0], -1)
@@ -100,41 +114,45 @@ class SimCLR(pl.LightningModule):
         logits = torch.cat([positives, negatives], dim=1)
         labels = torch.zeros(logits.shape[0], dtype=torch.long, device=logits.device)
         logits = logits / self.hparams.temperature
-        
-        loss = self.criterion(logits, labels)
 
-        # # Calculate cosine similarity
-        # cos_sim = F.cosine_similarity(feats[:, None, :], feats[None, :, :], dim=-1)
+        contrastive_loss = self.contrastive_loss(logits, labels)
 
-        # # Mask out cosine similarity to itself
-        # self_mask = torch.eye(cos_sim.shape[0], dtype=torch.bool, device=cos_sim.device)
-        # cos_sim.masked_fill_(self_mask, -9e15)
 
-        # # Find positive example -> batch_size//2 away from the original example
-        # pos_mask = self_mask.roll(shifts=cos_sim.shape[0]//2, dims=0)
+        ### DICE Loss ###
+        # pred_segm = self.unet(torch.concat([img1, img2]))
 
-        # # InfoNCE loss
-        # cos_sim = cos_sim / self.hparams.temperature
-        # nll = -cos_sim[pos_mask] + torch.logsumexp(cos_sim, dim=-1)
-        # nll = nll.mean()
+        dice_loss = self.dice_loss(pred_segm, torch.concat([lab1, lab2]))
+
+        final_loss = (1-self.dice_weight)*contrastive_loss + self.dice_weight*dice_loss
 
         # Logging loss
-        self.log(mode+'_loss', loss, prog_bar=True)
+        self.log(mode+'_final_loss', final_loss, prog_bar=True)
+        self.log(mode+'_dice_loss', dice_loss, prog_bar=True)
+        self.log(mode+'_contrastive_loss', contrastive_loss, prog_bar=True)
+        
         top1, top5 = self.accuracy(logits, labels, topk=(1, 5))
         # # Get ranking position of positive example
-        # comb_sim = torch.cat([cos_sim[pos_mask][:,None],  # First position positive example
-        #                       cos_sim.masked_fill(pos_mask, -9e15)],
-        #                      dim=-1)
-        # sim_argsort = comb_sim.argsort(dim=-1, descending=True).argmin(dim=-1)
 
-        # # Logging ranking metrics
+        # Logging ranking metrics
         self.log(mode+'_acc_top1', top1[0],
                  prog_bar=True)
         self.log(mode+'_acc_top5', top5[0],
                  prog_bar=True)
-        # self.log(mode+'_acc_mean_pos', 1+sim_argsort.float().mean())
-        return loss
-        # return nll
+
+        if mode == 'val':
+            self.val_dsc(pred_segm,
+                         torch.concat([lab1.squeeze(dim=1),
+                                       lab2.squeeze(dim=1)]).type(torch.long))
+            self.log(mode+'_dice', self.val_dsc.compute(), prog_bar=True,
+                     on_step=True, on_epoch=True, batch_size=8, logger=True)
+        elif mode == 'train':
+            self.train_dsc(pred_segm,
+                           torch.concat([lab1.squeeze(dim=1),
+                                         lab2.squeeze(dim=1)]).type(torch.long))
+            self.log(mode+'_dice', self.train_dsc.compute(), prog_bar=True,
+                     on_step=True, on_epoch=True, batch_size=16, logger=True)
+        return final_loss
+
     @staticmethod
     def accuracy(output, target, topk=(1,)):
         """Computes the accuracy over the k top predictions for the specified values of k"""
