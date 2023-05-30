8cea48d3091c99a9f6075acc3f720017197c272e
Vladyslav Zalevskyi <vzalevskyi24@gmail.com>
2023-05-30 15:20:08 +0200

    final report plots dump

--- scripts/mesh_segmentations.py
+++ scripts/mesh_segmentations.py
@@ -2,11 +2,11 @@ from pathlib import Path
 import subprocess
 from tqdm import tqdm
 
-segm_path = Path('/mrhome/vladyslavz/git/central-sulcus-analysis/data/via11/nobackup/segm_results/correctes_res')
-imgs = [x for x in segm_path.glob('**/*.nii.gz')]
+segm_path = Path('/mrhome/vladyslavz/git/central-sulcus-analysis/data/via11/nobackup/segm_results/correctes_res/predicted')
+imgs = [x for x in segm_path.glob('**/*sulc*.nii.gz')]
 print(len(imgs))
 for i in tqdm(imgs):
     o = str(i).replace('.nii.gz', '')
     o = o.replace('segmentations', 'meshes')
-    AimsMeshBrainL = ['AimsMeshBrain', '-i', str(i), '-o', f'{o}.ply']
+    AimsMeshBrainL = ['/home/vladyslavz/portable_apps/brainvisa4_5/bin/AimsMeshBrain', '-i', str(i), '-o', f'{o}.ply']
     subprocess.run(AimsMeshBrainL)
