41b33bca3e6938962a83547313543f7ba0a5adac
Vladyslav Zalevskyi <vzalevskyi24@gmail.com>
2023-07-18 16:15:02 +0200

    refactir CS_Shape_Dataset to output list of all subjects

---
+++ shape_features/dataloaders.py
@@ -0,0 +1,142 @@
+from pathlib import Path
+
+import open3d as o3d
+import pandas as pd
+import SimpleITK as sitk
+from torch.utils.data import Dataset
+from tqdm import tqdm
+
+
+class VIA11_Corrected_CS_Loader(Dataset):
+    def __init__(self,
+                 bv_good: bool = True,
+                 corrected: bool = True,
+                 all_bv: bool = False,
+                 preload: bool = False) -> None:
+        """Constructor for VIA11_Corrected_CS_Loader
+
+        Args:
+            bv_good (bool, optional): Flag to indicate wether to load BrainVISA segmentations
+                that are good (type 1). Defaults to True.
+            corrected (bool, optional): Flag to indicate wether to load the manually
+                corrected CS segmentations. Overrides any BrainVISA segmentations loaded. Defaults to True.
+            all_bv (bool, optional): Flag to indicate wether to load all BrainVISA segmentations no matter their QC status.
+                Overrides all other flags. Defaults to False.
+            preload: (bool, optional): Flag to indicate wether to preload the data in the 
+                _get_item() or to return the paths to images. Defaults to False.
+        """
+
+        self.bv_good = bv_good
+        self.corrected = corrected
+        self.all_bv = all_bv
+        self.preload = preload
+
+        # store segmentations in format 
+        # {subject_id: sub-viaxxx, type': type_of_segmentation},
+        #  'lsegm': path_to_left_segmentation, 'rsegm': path_to_right_segmentation, 
+        #  'lmesh': path_to_left_mesh, 'rmesh': path_to_right_mesh}
+        #               '}
+        self.cs_segmentations = []
+        self._load_data()
+
+    def __len__(self):
+        return len(self.cs_segmentations)
+
+    def __getitem__(self, idx):
+        if self.preload:
+            d = self.cs_segmentations[idx]
+            return {'subject_id': d['subject_id'],
+                    'type': d['type'],
+                    'lsegm': sitk.ReadImage(d['lsegm']),
+                    'rsegm': sitk.ReadImage(d['rsegm']),
+                    'lmesh': o3d.io.read_triangle_mesh(d['lmesh']),
+                    'rmesh': o3d.io.read_triangle_mesh(d['rmesh'])}
+        else:
+            return self.cs_segmentations[idx]
+
+    def _load_data(self):
+        corrected_loaded = []
+        if self.corrected:
+            corr_paths = Path('/mnt/projects/VIA_Vlad/nobackup/BrainVisa/CS_edited').glob('**/LSu*sub-via*.nii.gz')
+            for lsegmpath in corr_paths:
+                resgmpath = str(lsegmpath).replace('LSulci', 'RSulci')
+                lmeshpath = str(lsegmpath).replace('.nii.gz', '.ply')
+                rmeshpath = str(lmeshpath).replace('LSulci', 'RSulci')
+
+                subj = lsegmpath.name[7:17]
+                subj_data = {'subject_id': subj}
+                subj_data['lsegm'] = lsegmpath
+                subj_data['rsegm'] = resgmpath
+                subj_data['lmesh'] = lmeshpath
+                subj_data['rmesh'] = rmeshpath
+                subj_data['type'] = 'corrected'
+                subj_data['site'] = str(lsegmpath).split('/')[-2]
+                self.cs_segmentations.append(subj_data)
+                corrected_loaded.append(subj)
+
+        if self.bv_good:
+            # load QC results
+            qc_results = pd.read_excel(Path('/mnt/projects/VIA_Vlad/nobackup/QA_centralSulcus_lkj.xlsx'))
+            qc_results = qc_results.set_index('subjects')
+            bv_good_subjs = qc_results[qc_results.vis_QA == 1]
+
+            for subj, metadata in bv_good_subjs.iterrows():
+
+                # skip BrainVISA segmentations if corrected segmentations are loaded
+                if subj in corrected_loaded:
+                    continue
+
+                lsegmpath = f'/mnt/projects/VIA_Vlad/nobackup/BrainVisa/BrainVisa/{metadata.sites}/{subj}/t1mri/default_acquisition/default_analysis/folds/3.1/default_session_best/segmentation/LSulci_{subj}_default_session_best.nii.gz'
+                resgmpath = lsegmpath.replace('LSulci', 'RSulci')
+                lmeshpath = lsegmpath.replace('.nii.gz', '.ply')
+                rmeshpath = lmeshpath.replace('LSulci', 'RSulci')
+
+                subj_data = {'subject_id': subj}
+                subj_data['lsegm'] = lsegmpath
+                subj_data['rsegm'] = resgmpath
+                subj_data['lmesh'] = lmeshpath
+                subj_data['rmesh'] = rmeshpath
+                subj_data['type'] = 'bv_good'
+                subj_data['site'] = metadata.sites
+                self.cs_segmentations.append(subj_data)
+
+        if self.all_bv:
+            self.cs_segmentations = []
+            # load QC results
+            qc_results = pd.read_excel(Path('/mnt/projects/VIA_Vlad/nobackup/QA_centralSulcus_lkj.xlsx'))
+            qc_results = qc_results.set_index('subjects')
+            bv_good_subjs = qc_results[qc_results.vis_QA != 999]
+
+            for subj, metadata in bv_good_subjs.iterrows():
+
+                lsegmpath = f'/mnt/projects/VIA_Vlad/nobackup/BrainVisa/BrainVisa/{metadata.sites}/{subj}/t1mri/default_acquisition/default_analysis/folds/3.1/default_session_best/segmentation/LSulci_{subj}_default_session_best.nii.gz'
+                resgmpath = lsegmpath.replace('LSulci', 'RSulci')
+                lmeshpath = lsegmpath.replace('.nii.gz', '.ply')
+                rmeshpath = lmeshpath.replace('LSulci', 'RSulci')
+
+                subj_data = {'subject_id': subj}
+                subj_data['lsegm'] = lsegmpath
+                subj_data['rsegm'] = resgmpath
+                subj_data['lmesh'] = lmeshpath
+                subj_data['rmesh'] = rmeshpath
+                subj_data['type'] = 'bv_good'
+                subj_data['site'] = metadata.sites
+                self.cs_segmentations.append(subj_data)
+
+    def get_subjects(self):
+        """Returns a list of all subjects in the dataset
+
+        Returns:
+            list: ['left|right', 'sub-viaxxx', 'corrected|bv', 'sitk_image']
+        """
+        sulci = [self.__getitem__(i) for i in tqdm(range(len(self.cs_segmentations)))]
+
+        # unravell all the sulci
+        sulci_list = []
+        for s in sulci:
+            lsegm = s['lsegm'] if self.preload else sitk.ReadImage(s['lsegm'])
+            rsegm = s['rsegm'] if self.preload else sitk.ReadImage(s['rsegm'])
+
+            sulci_list.append(['left', s['subject_id'], s['type'], lsegm])
+            sulci_list.append(['right', s['subject_id'], s['type'], rsegm])
+        return sulci_list
---
+++ shape_features/shape_utils.py
@@ -0,0 +1,51 @@
+import numpy as np
+import multiprocessing as mp
+
+import trimesh
+import SimpleITK as sitk
+
+
+def process_sulc_pair(x):
+    """Calculates the minimum distance between two sulci
+        registered with ICP to each other and the sulcus pair name
+
+    Args:
+        x (tuple[list]): sulcus pair, where each sulcus is a list of
+            [hemi, subj, type, sitk_segm] as the output of get_subjects()
+
+    Returns:
+        tuple: (min_dist, sulcus_pair_name)
+    """
+    sulc, sulc2 = x
+    # register the sulci min(a->b or b->a)
+
+    # need to flip if different hemispheres
+    if sulc[0] != sulc2[0]:
+        sulc2[3] = sitk.Flip(sulc2[3], [False, False, True])
+
+    # register the sulci with ICP
+    s1_points = np.stack(np.where(sitk.GetArrayFromImage(sulc[3]))).T
+    s2_points = np.stack(np.where(sitk.GetArrayFromImage(sulc2[3]))).T
+
+    # finds the transformation matrix sending a to b
+    _, __, cost_s1_to_s2 = trimesh.registration.icp(a=s1_points, b=s2_points, max_iterations=1000)
+    _, __, cost_s2_to_s1 = trimesh.registration.icp(a=s2_points, b=s1_points, max_iterations=1000)
+
+    return min(cost_s1_to_s2, cost_s2_to_s1), f'{sulc[0]}_{sulc[1]}-{sulc2[0]}_{sulc2[1]}'
+
+
+def sulcus_dist(sulc: list, sulclist: list[list]):
+    # create a vector of pairwise distances between a given sulcus and all sulci
+    dists = []
+    reg_keys = []
+
+    sulc_pairs_list = [(sulc, sulc2) for sulc2 in sulclist]
+
+    with mp.Pool(mp.cpu_count()) as p:
+        r = list(p.imap(process_sulc_pair, sulc_pairs_list))
+
+    for d, k in r:
+        dists.append(d)
+        reg_keys.append(k)
+
+    return dists, reg_keys
